1b51d1bcf6fff8a90178f7be6979cf5778fadd46
obsessed <obsessed776@gmail.com>
2026-07-12 23:28:25 +0300

    add Jinja2 templates configuration

--- src/config.py
+++ src/config.py
@@ -5,6 +5,8 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 BASE_DIR = Path(__file__).resolve().parent
 
 class Settings(BaseSettings):
+    TEMPLATES_DIR: Path = BASE_DIR / "templates"
+
     model_config = SettingsConfigDict(env_file=BASE_DIR / ".env")
 
 
--- src/main.py
+++ src/main.py
@@ -1,3 +1,7 @@
 from fastapi import FastAPI
+from fastapi.templating import Jinja2Templates
+
+from src.config import settings
 
 app = FastAPI()
+templates = Jinja2Templates(directory=settings.TEMPLATES_DIR)
