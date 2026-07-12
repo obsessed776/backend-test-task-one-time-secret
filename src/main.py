f6da6fd6a7b6e152fa4cbacba54447c8de795999
obsessed <obsessed776@gmail.com>
2026-07-12 23:55:21 +0300

    add lifespan for creating tables in database

--- src/main.py
+++ src/main.py
@@ -1,10 +1,22 @@
+from collections.abc import AsyncGenerator
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
 from src.config import settings
+from src.database.db import engine
+from src.database.models import Base
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(_: FastAPI) -> AsyncGenerator[None, None]:
+    Base.metadata.create_all(bind=engine)
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 templates = Jinja2Templates(directory=settings.TEMPLATES_DIR)
 
 
