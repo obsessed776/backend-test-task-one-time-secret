023dcd0178125065e56e22d9518c466f832a2bd4
obsessed <obsessed776@gmail.com>
2026-07-12 23:30:37 +0300

    add test view function

--- src/main.py
+++ src/main.py
@@ -1,7 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
 from src.config import settings
 
 app = FastAPI()
 templates = Jinja2Templates(directory=settings.TEMPLATES_DIR)
+
+
+@app.get("/", response_class=HTMLResponse)
+def index(request: Request):
+    return templates.TemplateResponse(request, "index.html")
